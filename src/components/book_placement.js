85521f4dea55ae0e2c6c1cd63afda043a043722c
Iulia Mogos <iulia.mogos@king.com>
2019-03-22 15:11:23 +0100

    placement

--- src/components/book_placement.js
+++ src/components/book_placement.js
@@ -1,29 +1,47 @@
 import React, { Component } from 'react'
 import { Text, StyleSheet, Button } from 'react-native'
+import firebase from 'firebase'
 
 export default class BookPlacement extends Component {
-    onAssignToMePressed = ()=> {
+
+    constructor(props) {
+        super(props);
+        this.holderRef = "";
+        this.ownerRef = "";
+        const { holder, owner } = this.props;
+        firebase.database().ref("users/" + holder).once('value').then(function (snapshot) {
+            this.holderRef = snapshot.val().email;
+        });
+        firebase.database().ref("users/" + owner).once('value').then(function (snapshot) {
+            this.ownerRef = snapshot.val().email;
+        });
+    }
+
+    onAssignToMePressed = () => {
         this.props.callback();
     }
     isBookAvailableForPlacement() {
-        const {holder, owner, userdata} = this.props;
-        return userdata.name !== owner && holder ==='';
+        const { userdata } = this.props;
+        return userdata.email !== this.ownerRef.email && this.props.holder === -1;
     }
     render() {
         let content;
         if (this.isBookAvailableForPlacement()) {
-            content = (<Text style={styles.description}> <br /> Owner: {this.props.owner}
-                <br/><Button style={styles.button} title="Assign to me" color="#000000ff" onPress={this.onAssignToMePressed} />
-                </Text>);  
-            
-        } else {
-            let holder =  this.props.holder;
-            if (holder ==='')
-                holder = this.props.owner;
+            content = (<Text style={styles.description}> <br /> Owner: {this.ownerRef.name}
+                <br /><Button style={styles.button} title="Assign to me" color="#000000ff" onPress={this.onAssignToMePressed} />
+            </Text>);
 
-            content = (<Text style={styles.description}> <br /> Owner: {this.props.owner}
-                  <br/><Button style={styles.button} title={holder} color="#808080" />
-                 </Text>);
+        } else {
+            let holder = "";
+            if (this.props.holder === -1) {
+                holder = this.ownerRef.name;
+            } else {
+                holder = this.holderRef.name;
+            }
+
+            content = (<Text style={styles.description}> <br /> Owner: {this.ownerRef.name}
+                <br /><Button style={styles.button} title={holder} color="#808080" />
+            </Text>);
         }
         return (
             content
@@ -34,7 +52,7 @@ export default class BookPlacement extends Component {
 const styles = StyleSheet.create({
     button:
     {
-       flex:2,
+        flex: 2,
     },
     description:
     {
@@ -42,6 +60,6 @@ const styles = StyleSheet.create({
         color: 'gray',
         fontWeight: 'normal',
         flexDirection: 'column',
-        justifyContent:'space-between'
+        justifyContent: 'space-between'
     }
 });
