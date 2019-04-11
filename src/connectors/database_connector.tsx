b2f4b7356b7718012b5a7e96c894fa7f517ad6db
amogos <iulia.enes5@gmail.com>
2019-04-11 13:25:24 +0200

    types for db

--- src/connectors/database_connector.tsx
+++ src/connectors/database_connector.tsx
@@ -1,19 +1,21 @@
+import * as Types from "../types"
+
 export default class DatabaseConnector {
     constructor() {
         if (new.target === DatabaseConnector) {
             throw new TypeError("__abstract_construct_implementation_required__");
         }
     }
-    getBooks(onComplete:any) {
+    getBooks(onComplete: any) {
         throw new Error('__abstract_not_implemented__');
     }
-    assignBook(data:any, user:any, onComplete:any) {
+    assignBook(data: Types.BookKeyType, user: Types.UserType, onComplete: any) {
         throw new Error('__abstract_not_implemented__');
     }
-    deleteBook(data:any, onComplete:any) {
+    deleteBook(data: Types.BookKeyType, onComplete: any) {
         throw new Error('__abstract_not_implemented__');
     }
-    addBook(data:any, user:any, onComplete:any) {
+    addBook(data: Types.BookValueType, onComplete: any) {
         throw new Error('__abstract_not_implemented__');
     }
 
