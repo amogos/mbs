949bb6254981939977ae3cde319dbbbd5cc4c082
amogos <iulia.enes5@gmail.com>
2019-05-03 13:53:11 +0200

    small ref

--- src/reducers/tree_reducer.ts
+++ src/reducers/tree_reducer.ts
@@ -1,4 +1,3 @@
-import * as Types from "../types";
 import {
     ADD_BOOK,
     LIST_BOOKS,
@@ -9,33 +8,22 @@ import {
 import FirebaseConnector from '../connectors/firebase_connector'
 import FacebookConnector from '../connectors/facebook_connector'
 
-
-var databaseConnector = new FirebaseConnector()
-var socialConnector = new FacebookConnector()
-
-
 const initialState = {
     screen: SHOW_BLANK,
-    counter: 0,
-    id: 0,
-    dbconnector: databaseConnector,
-    socialconnector: socialConnector
+    dbconnector: new FirebaseConnector(),
+    socialconnector: new FacebookConnector()
 }
-
 
 export default function tree(state = initialState, action: any) {
     switch (action.type) {
         case ADD_BOOK:
             return state;
         case LIST_BOOKS:
-           return state;
+            return state;
         case USER_DATA:
-            let test =  Object.assign({}, state, {
+            return Object.assign({}, state, {
                 userdata: action.userdata
             })
-            alert(JSON.stringify(test))
-            return test;
-
         default:
             return state;
     }
