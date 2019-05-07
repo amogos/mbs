eca31484d95ec5c2d7a3b8e9621595b906ca4ceb
amogos <iulia.enes5@gmail.com>
2019-05-07 09:00:18 +0200

    export store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,11 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import rootReducer from './reducers/index'
+import Store from './store'
 import MainContainer from './containers/main_container';
 
-const store = createStore(rootReducer)
-
 ReactDom.render(
-    <Provider store={store}>
+    <Provider store={Store}>
         <MainContainer />
     </Provider>
     , document.getElementById("root"));
