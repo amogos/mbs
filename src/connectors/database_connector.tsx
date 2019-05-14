9e396fc868ce511ae35d3a737d22ea6c17fdcf48
amogos <iulia.enes5@gmail.com>
2019-05-14 06:04:15 +0200

    string instead of key type

--- src/connectors/database_connector.tsx
+++ src/connectors/database_connector.tsx
@@ -3,6 +3,6 @@ import * as DataTypes from "../types"
 export default interface DatabaseConnector {
     querryBooks(onComplete?: () => void): Array<DataTypes.BookRecordType>;
     assignBook(index: number, user: DataTypes.UserType, onComplete?: () => void): void;
-    deleteBook(data: DataTypes.BookKeyType, onComplete?: () => void): void;
+    deleteBook(bookKey: string, onComplete?: () => void): void;
     addBook(data: DataTypes.BookValueType, onComplete?: () => void): void;
 }
