bebff9e9ea187648b0c9e5c5c737640e7f177a4c
amogos <iulia.enes5@gmail.com>
2019-05-17 16:28:26 +0200

    lint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 // src/index.js
-import React from 'react'
-import ReactDom from 'react-dom'
-import { Provider } from 'react-redux'
-import Store from './store'
-import BannerContainer from './containers/banner_container';
-import MainContainer from './containers/main_container';
-import ConfirmationDialogContainer from './containers/confirmation_dialog_container';
+import React from 'react'  // eslint-disable-line no-unused-vars
+import ReactDom from 'react-dom'// eslint-disable-line no-unused-vars
+import { Provider } from 'react-redux'// eslint-disable-line no-unused-vars
+import Store from './store'// eslint-disable-line no-unused-vars
+import BannerContainer from './containers/banner_container';// eslint-disable-line no-unused-vars
+import MainContainer from './containers/main_container';// eslint-disable-line no-unused-vars
+import ConfirmationDialogContainer from './containers/confirmation_dialog_container';// eslint-disable-line no-unused-vars
 
 ReactDom.render(
     <Provider store={Store}>
