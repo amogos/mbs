081565eaada73737ef910bef9d5174316dc280fe
amogos <iulia.enes5@gmail.com>
2019-05-21 06:06:33 +0200

    add_book spec

---
+++ src/actions/__tests__/add_book.spec.ts
@@ -0,0 +1,29 @@
+import * as ActionTypes from '../../constants/action_constant'
+import * as Actions from '../index'
+import * as DataTypes from '../../types'
+
+const bookValue: DataTypes.BookValueType = {
+    "author": "Eric Carle",
+    "holder": {
+        "email": "",
+        "name": ""
+    },
+    "image": "https://images-na.ssl-images-amazon.com/images/I/51lsugWtCvL._SY498_BO1,204,203,200_.jpg",
+    "language": "English",
+    "owner": {
+        "email": "daosmistique@yahoo.com",
+        "name": "Iulia Mogos"
+    },
+    "title": "The Very Hungry Caterpillar's ABC"
+};
+
+
+describe('addBook', () => {
+    it('should build well formatted action', () => {
+        const expectedAction = {
+            type: ActionTypes.ACTION_ADD_BOOK,
+            data: bookValue
+        }
+        expect(Actions.addBook(bookValue)).toEqual(expectedAction)
+    })
+})
