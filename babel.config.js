ec219a86a5dcc49667b5868b2f4130168f228b5d
amogos <iulia.enes5@gmail.com>
2019-06-07 06:25:21 +0200

    fix css jest#

--- babel.config.js
+++ babel.config.js
@@ -2,8 +2,5 @@ module.exports = function(api) {
     api.cache(true);
     return {
         presets: ['babel-preset-expo'],
-        plugins: [
-            ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], // `style: true` for less
-        ],
     };
 };
--- src/components/__tests__/social_login_component.spec.tsx
+++ src/components/__tests__/social_login_component.spec.tsx
@@ -1,21 +1,19 @@
-import SocialLoginComponent from '../social_login_component'
-import * as DataTypes from '../../types'
+import SocialLoginComponent from '../social_login_component';
+import * as DataTypes from '../../types';
 import renderer from 'react-test-renderer';
 
-jest.mock('./../../containers/facebook_container', () => 'login button')
+jest.mock('./../../containers/facebook_container', () => 'login button');
 
 test('Render username if logged in', () => {
     const component = renderer.create(
-        SocialLoginComponent({ userdata: { name: "mockuser", email: "mockusr@gmail.com" } })
+        SocialLoginComponent({ userdata: { name: 'mockuser', email: 'mockusr@gmail.com' } }),
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
 
 test('Render login button if not logged in yet', () => {
-    const component = renderer.create(
-        SocialLoginComponent({ userdata: DataTypes.nullUser })
-    );
+    const component = renderer.create(SocialLoginComponent({ userdata: DataTypes.nullUser }));
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
 });
