7ab0d0251f8aa857d5720a35541299683d19f70b
amogos <iulia.enes5@gmail.com>
2019-07-30 05:52:37 +0200

    tabs

--- src/components/banner_component.tsx
+++ src/components/banner_component.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import SocialLoginContainer from './../containers/social_login_container';
-import { PageHeader, Button, Avatar } from 'antd';
+import { PageHeader, Button } from 'antd';
 import FilteringTabsContainer from '../containers/filtering_tabs_container';
 import * as DataTypes from './../types';
 
@@ -14,6 +14,67 @@ interface Props {
 interface FilterProps {
     page: string;
 }
+
+interface TabProps {
+    setPage(page: string): void;
+    parentProps: Props;
+}
+
+const MySpaceTab = (props: TabProps) => {
+    const clickFunction = () => {
+        props.parentProps.gotoListBooks([]);
+        props.setPage('my-space');
+    };
+    return (
+        <Button type="link" onClick={clickFunction}>
+            <img src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg" alt="start" /> MySpace
+        </Button>
+    );
+};
+
+const SpacesTab = (props: TabProps) => {
+    const clickFunction = () => {
+        props.parentProps.gotoAddBook();
+        props.setPage('spaces');
+    };
+    return (
+        <Button type="link" onClick={clickFunction}>
+            <img src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg" alt="start" /> Spaces
+        </Button>
+    );
+};
+
+const RentTab = (props: TabProps) => {
+    const clickFunction = () => {
+        props.parentProps.gotoListBooks(['owner!=' + props.parentProps.userdata.id]);
+        props.setPage('rent');
+    };
+    return (
+        <Button type="link" onClick={clickFunction}>
+            <img src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg" alt="start" /> Rent
+        </Button>
+    );
+};
+
+const NotificationsTab = (props: TabProps) => {
+    const clickFunction = () => {
+        props.parentProps.gotoNotifications();
+        props.setPage('notifications');
+    };
+    return (
+        <Button type="link" onClick={clickFunction}>
+            <img src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg" alt="start" /> Notifications
+        </Button>
+    );
+};
+
+const SocialTab = (props: TabProps) => {
+    return (
+        <Button type="link">
+            <SocialLoginContainer />
+        </Button>
+    );
+};
 
 const ShowFilteringTabs = (props: FilterProps) => {
     if (props.page === 'rent' || props.page === 'my-space') {
@@ -29,61 +90,11 @@ const BannerComponent = (props: Props) => {
             <div className="wrap">
                 <div className="content">
                     <p className="contentLink">
-                        <Button
-                            type="link"
-                            onClick={() => {
-                                props.gotoListBooks([]);
-                                setPage('my-space');
-                            }}
-                        >
-                            <img
-                                src="https://gw.alipayobjects.com/zos/rmsportal/MjEImQtenlyueSmVEfUD.svg"
-                                alt="start"
-                            />{' '}
-                            MySpace
-                        </Button>
-                        <Button
-                            type="link"
-                            onClick={() => {
-                                props.gotoAddBook();
-                                setPage('spaces');
-                            }}
-                        >
-                            <img
-                                src="https://gw.alipayobjects.com/zos/rmsportal/NbuDUAuBlIApFuDvWiND.svg"
-                                alt="start"
-                            />{' '}
-                            Spaces
-                        </Button>
-                        <Button
-                            type="link"
-                            onClick={() => {
-                                props.gotoListBooks(['owner!=' + props.userdata.id]);
-                                setPage('rent');
-                            }}
-                        >
-                            <img
-                                src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
-                                alt="start"
-                            />{' '}
-                            Rent
-                        </Button>
-                        <Button
-                            type="link"
-                            onClick={() => {
-                                props.gotoNotifications();
-                                setPage('notifications');
-                            }}
-                        >
-                            <img
-                                src="https://gw.alipayobjects.com/zos/rmsportal/ohOEPSYdDTNnyMbGuyLb.svg"
-                                alt="start"
-                            />{' '}
-                            Notifications
-                        </Button>
-                        <Button type="link">
-                            <SocialLoginContainer />
-                        </Button>
+                        <MySpaceTab parentProps={props} setPage={(page: string) => setPage(page)} />
+                        <SpacesTab parentProps={props} setPage={(page: string) => setPage(page)} />
+                        <RentTab parentProps={props} setPage={(page: string) => setPage(page)} />
+                        <NotificationsTab parentProps={props} setPage={(page: string) => setPage(page)} />
+                        <SocialTab parentProps={props} setPage={(page: string) => setPage(page)} />
                     </p>
                 </div>
                 <ShowFilteringTabs page={page} />
