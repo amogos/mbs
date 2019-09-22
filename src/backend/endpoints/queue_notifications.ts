a6411b65e1618248300b406b99e160caacabc416
iulia mogos <iulia.enes5@gmail.com>
2019-09-22 18:05:12 +0200

    remove waiter from queue

--- src/backend/endpoints/queue_notifications.ts
+++ src/backend/endpoints/queue_notifications.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { urlQueues } from './constants';
 import * as DataTypes from '../../types';
-import AsyncCallsWaiter from '../utils/async_calls_waiter';
 import { getUserRecordTypeFromId } from './user';
 import { getBookRawRecordTypeFromId } from './books';
 
@@ -9,36 +8,36 @@ export async function getQueueNotifications(
     user: DataTypes.UserRecordType,
     onError: (resultCode: number) => void,
 ): Promise<DataTypes.QueueNotificationRecordType[]> {
-    let waiter = new AsyncCallsWaiter();
     let rentalNotifications: DataTypes.QueueNotificationRecordType[] = [];
-    await axios
+    let responseArray: any = null;
+
+    await axios                                                   
         .get(urlQueues + '?ownerId=' + user.id)
         .then(response => {
             if (response.data.length === 0) return rentalNotifications;
-
-            response.data.forEach(async (item: any) => {
-                waiter.begin();
-                const user = await getUserRecordTypeFromId(item.userId, onError);
-                const rawBook = await getBookRawRecordTypeFromId(item.bookId, onError);
-                const notAssigned = rawBook.holder < 0;
-                const alreadyOneRequestForBookIdProcessed = rentalNotifications.find(
-                    notifiction => notifiction.bookId === item.bookId,
-                );
-                if (notAssigned && !alreadyOneRequestForBookIdProcessed) {
-                    let notification: DataTypes.QueueNotificationRecordType = {
-                        id: item.id,
-                        bookTitle: rawBook.title,
-                        bookId: item.bookId,
-                        user: user,
-                        duration: item.duration,
-                    };
-                    rentalNotifications.push(notification);
-                }
-                waiter.end();
-            });
+            responseArray = response.data;
         })
         .catch(error => onError(error));
 
-    await waiter.result();
+    for (let i = 0; i < responseArray.length; i++) {
+        const item = responseArray[i];
+        const user = await getUserRecordTypeFromId(item.userId, onError);
+        const rawBook = await getBookRawRecordTypeFromId(item.bookId, onError);
+        const notAssigned = rawBook.holder < 0;
+        const alreadyOneRequestForBookIdProcessed = rentalNotifications.find(
+            notifiction => notifiction.bookId === item.bookId,
+        );
+        if (notAssigned && !alreadyOneRequestForBookIdProcessed) {
+            let notification: DataTypes.QueueNotificationRecordType = {
+                id: item.id,
+                bookTitle: rawBook.title,
+                bookId: item.bookId,
+                user: user,
+                duration: item.duration,
+            };
+            rentalNotifications.push(notification);
+        }
+
+    }
     return rentalNotifications;
 }
