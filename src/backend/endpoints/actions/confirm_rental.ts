37553ac473ec774cd64424124dcb16ce897a2630
iulia mogos <iulia.enes5@gmail.com>
2019-09-23 08:19:03 +0200

    remove waiter completely

--- src/backend/endpoints/actions/confirm_rental.ts
+++ src/backend/endpoints/actions/confirm_rental.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import * as DataTypes from '../../../types';
 import { urlBooks, urlQueues, OneDayMilliseconds } from '../constants';
-import AsyncCallsWaiter from '../../utils/async_calls_waiter';
 import * as BookStateTypes from '../../../constants/book_states_constant';
 
 export async function confirmRental(
@@ -10,32 +9,30 @@ export async function confirmRental(
     onError: (resultCode: number) => void,
 ): Promise<boolean> {
     const bookIdUrl = `${urlBooks}/${rental.bookId}`;
-    let waiter = new AsyncCallsWaiter();
+    let bookResponse: any = null;
 
     await axios
         .get(bookIdUrl)
-        .then(async result => {
-            waiter.begin();
-            const value = {
-                ...result.data,
-                state: BookStateTypes.default.STATE_BOOK_IN_TRANSIT_TO_HOLDER,
-                holder: rental.user.id,
-                return: Date.now() + rental.duration * OneDayMilliseconds,
-            };
-            await axios
-                .put(bookIdUrl, value)
-                .then(() => waiter.end())
-                .catch(error => {
-                    onError(error);
-                    return false;
-                });
-        })
+        .then(response => bookResponse = response)
         .catch(error => {
             onError(error);
             return false;
         });
 
-    await waiter.result();
+    if (!bookResponse) return false;
+
+    const value = {
+        ...bookResponse.data,
+        state: BookStateTypes.default.STATE_BOOK_IN_TRANSIT_TO_HOLDER,
+        holder: rental.user.id,
+        return: Date.now() + rental.duration * OneDayMilliseconds,
+    };
+    await axios
+        .put(bookIdUrl, value)
+        .catch(error => {
+            onError(error);
+            return false;
+        });
 
     await axios
         .delete(urlQueues + '/' + rental.id)
