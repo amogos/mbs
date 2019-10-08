518fc0f697075b682cc849c6226a9a4d3287d354
amogos <iulia.enes5@gmail.com>
2019-10-08 09:59:45 +0200

    wip

---
+++ src/frontend/components/errors/page_not_found.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { withStyle } from './../aux_component';
+
+const PageNotFound = () => {
+    return <p>Page not found</p>;
+};
+export default withStyle(PageNotFound, 'page_not_found');
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // src/index.js
 import React from 'react';
 import ReactDom from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import 'antd/dist/antd.css';
 import './frontend/components/common.css';
@@ -9,6 +10,7 @@ import './frontend/components/banner/banner.css';
 import './frontend/components/books/books.css';
 import './frontend/components/spaces/spaces.css';
 import './frontend/components/social/social.css';
+import './frontend/components/errors/errors.css';
 
 import { Provider } from 'react-redux';
 import Store from './frontend/store';
@@ -16,13 +18,29 @@ import BannerContainer from './frontend/containers/banner_container';
 import MainContainer from './frontend/containers/main_container';
 import RightContainer from './frontend/containers/right_container';
 import TopContainer from './frontend/containers/top_container';
+import PageNotFound from './frontend/components/errors/page_not_found';
+
+import Aux from './frontend/components/aux_component';
+
+const App = () => {
+    return (
+        <Aux>
+            <BannerContainer />
+            <MainContainer />
+            <RightContainer />
+            <TopContainer />
+        </Aux>
+    );
+};
 
 ReactDom.render(
     <Provider store={Store}>
-        <BannerContainer />
-        <MainContainer />
-        <RightContainer />
-        <TopContainer />
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route component={PageNotFound} />
+            </Switch>
+        </Router>
     </Provider>,
     document.getElementById('root'),
 );
