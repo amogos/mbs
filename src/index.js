7bfea1d483c105c6db45063c24c199532ef7438f
amogos <iulia.enes5@gmail.com>
2019-10-08 11:15:29 +0200

    wip

--- src/index.js
+++ src/index.js
@@ -2,18 +2,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
-import 'antd/dist/antd.css';
-import './frontend/components/common.css';
-import './frontend/components/regions/regions.css';
-import './frontend/components/banner/banner.css';
-import './frontend/components/books/books.css';
-import './frontend/components/spaces/spaces.css';
-import './frontend/components/social/social.css';
-import './frontend/components/errors/errors.css';
-
 import { Provider } from 'react-redux';
 import Store from './frontend/store';
+
+import 'antd/dist/antd.css';
+import './main.css';
+
 import BannerContainer from './frontend/containers/banner_container';
 import MainContainer from './frontend/containers/main_container';
 import RightContainer from './frontend/containers/right_container';
