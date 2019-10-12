320f0c7a33019b7ad0338b77fbe06cff26e5ea2b
amogos <iulia.enes5@gmail.com>
2019-10-12 14:33:08 +0200

    wip

--- src/frontend/middleware/dispatch_cacher.js
+++ src/frontend/middleware/dispatch_cacher.js
@@ -2,32 +2,32 @@ import * as ActionConstants from './../../shared/constants/action_constant';
 
 const { PageActionConstant } = ActionConstants.default;
 
-const dispatchCacher = store => next => action => {
+const cacheBookListing = (store, action) => {
     let filters = sessionStorage.getItem('filters');
 
+    const filterExists =
+        filters && filters.ACTION_GOTO_LIST_BOOKS && filters.ACTION_GOTO_LIST_BOOKS === JSON.stringify(action.filters);
+
+    if (filterExists) {
+        return Object.assign({}, store.getState(), {
+            action: PageActionConstant.ACTION_GOTO_LIST_BOOKS,
+        });
+    } else {
+        if (!filters) {
+            filters = {};
+        }
+        if (!filters.ACTION_GOTO_LIST_BOOKS) {
+            filters = { ...filters, ACTION_GOTO_LIST_BOOKS: '' };
+        }
+        filters.ACTION_GOTO_LIST_BOOKS = JSON.stringify(action.filters);
+        sessionStorage.setItem('filters', filters);
+    }
+};
+
+const dispatchCacher = store => next => action => {
     switch (action.type) {
         case PageActionConstant.ACTION_GOTO_LIST_BOOKS:
-            {
-                const filterExists =
-                    filters &&
-                    filters.ACTION_GOTO_LIST_BOOKS &&
-                    filters.ACTION_GOTO_LIST_BOOKS === JSON.stringify(action.filters);
-
-                if (filterExists) {
-                    return Object.assign({}, store.getState(), {
-                        action: PageActionConstant.ACTION_GOTO_LIST_BOOKS,
-                    });
-                } else {
-                    if (!filters) {
-                        filters = {};
-                    }
-                    if (!filters.ACTION_GOTO_LIST_BOOKS) {
-                        filters = { ...filters, ACTION_GOTO_LIST_BOOKS: '' };
-                    }
-                    filters.ACTION_GOTO_LIST_BOOKS = JSON.stringify(action.filters);
-                    sessionStorage.setItem('filters', filters);
-                }
-            }
+            cacheBookListing(store, action);
             break;
     }
 
