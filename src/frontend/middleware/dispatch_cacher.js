cbd2fdf5e304f17f7f7de861d5d1bd2a0bb3ee6f
amogos <iulia.enes5@gmail.com>
2019-10-12 16:05:57 +0200

    wip

--- src/frontend/middleware/dispatch_cacher.js
+++ src/frontend/middleware/dispatch_cacher.js
@@ -5,25 +5,26 @@ const { PageActionConstant } = ActionConstants.default;
 
 let bookCache = new QueryCache(10);
 
-const cacheBookListing = (store, action, next) => {
-    let cacheKey = '';
-    action.filters.forEach(element => (cacheKey = cacheKey + element));
-    const cacheEntry = bookCache.getEntry(cacheKey);
-
-    if (cacheEntry) {
-        return Object.assign({}, store.getState(), {
-            action: PageActionConstant.ACTION_GOTO_LIST_BOOKS,
-        });
-    } else {
-        bookCache.addEntry(cacheKey, null);
-        return next(action);
-    }
-};
-
 const dispatchCacher = store => next => action => {
+    let cacheKey = '';
+    if (action.filters) {
+        action.filters.forEach(element => (cacheKey = cacheKey + element));
+    }
+
     switch (action.type) {
         case PageActionConstant.ACTION_GOTO_LIST_BOOKS:
-            return cacheBookListing(store, action, next);
+            {
+                const cacheEntry = bookCache.getEntry(cacheKey);
+
+                if (cacheEntry) {
+                    return Object.assign({}, store.getState(), {
+                        action: PageActionConstant.ACTION_GOTO_LIST_BOOKS,
+                    });
+                } else {
+                    bookCache.addEntry(cacheKey, null);
+                }
+            }
+            break;
     }
 
     return next(action);
