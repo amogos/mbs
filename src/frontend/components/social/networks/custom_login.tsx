8cc467f5277b82c657a47f8a507877d606afe7e6
amogos <iulia.enes5@gmail.com>
2019-11-14 12:37:22 +0100

    useINput

--- src/frontend/components/social/networks/custom_login.tsx
+++ src/frontend/components/social/networks/custom_login.tsx
@@ -2,29 +2,33 @@ import React from 'react';
 import * as DataTypes from '../../../../shared/types';
 import { Button, Input, Icon } from 'antd';
 import { SocialNetwork } from './../../../../shared/constants/social_networks_constants';
+import { useInput } from './../../hooks/use_input';
 
 interface Props {
     loginUser(userInfo: DataTypes.UserValueType): void;
     logoutUser(): void;
 }
+
+const fields = { username: '', password: '' };
+
 const CustomLogin = (props: Props) => {
     return (
         <div className="custom_login">
-            <div>
-                <Input
-                    style={{ width: '335px' }}
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    placeholder="Email"
-                />
-            </div>
-            <div>
-                <Input
-                    style={{ width: '335px' }}
-                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                    type="password"
-                    placeholder="Password"
-                />
-            </div>
+            <Input
+                style={{ width: '335px' }}
+                prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                {...useInput('Email', '', (value: string) => (fields.username = value))}
+            />
+
+            <Input
+                style={{ width: '335px' }}
+                prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                type="password"
+                {...useInput('Password', '', (value: string) => {
+                    fields.password = value;
+                })}
+            />
+
             <Button
                 className="custom_button"
                 onClick={() => {
