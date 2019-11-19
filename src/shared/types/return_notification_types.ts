1cff5e6083819ce8c1227751363f5aae0efe67c8
amogos <iulia.enes5@gmail.com>
2019-11-19 14:43:47 +0100

    wip

--- src/shared/types/return_notification_types.ts
+++ src/shared/types/return_notification_types.ts
@@ -1,4 +1,4 @@
-import { UserRecordType } from './user_types';
+import { UserRecordType, NullUserRecordType } from './user_types';
 
 export interface ReturnNotificationRecordType {
     id: number;
@@ -7,11 +7,24 @@ export interface ReturnNotificationRecordType {
     ownerId: number;
 }
 
+export const NullReturnNotificationRecordType: ReturnNotificationRecordType = {
+    id: 0,
+    bookId: 0,
+    userId: 0,
+    ownerId: 0,
+};
+
 export interface ReturnNotificationValueType {
     bookId: number;
     userId: number;
     ownerId: number;
 }
+
+export const NullReturnNotificationValueType: ReturnNotificationValueType = {
+    bookId: 0,
+    userId: 0,
+    ownerId: 0,
+};
 
 export interface ReturnNotificationType {
     returnId: number;
@@ -19,3 +32,10 @@ export interface ReturnNotificationType {
     bookTitle: string;
     user: UserRecordType;
 }
+
+export const NullReturnNotificationType: ReturnNotificationType = {
+    returnId: 0,
+    bookId: 0,
+    bookTitle: '',
+    user: NullUserRecordType,
+};
