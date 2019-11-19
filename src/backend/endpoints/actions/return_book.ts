21ba17283d8a1bb8622d94f91440fddf503d21e7
amogos <iulia.enes5@gmail.com>
2019-11-19 13:01:07 +0100

    fix return book

--- src/backend/endpoints/actions/return_book.ts
+++ src/backend/endpoints/actions/return_book.ts
@@ -1,17 +1,20 @@
 import axios from 'axios';
 import { urlBooks } from '../constants';
 import * as BookStateTypes from '../../../shared/constants/book_states_constant';
+import * as DataTypes from './../../../shared/types';
 
 export async function returnBook(bookId: number, onError: (resultCode: number) => void) {
+    const bookUrl = `${urlBooks}/${bookId}`;
     await axios
-        .get(urlBooks + '/' + bookId)
+        .get(bookUrl)
         .then(async result => {
-            const value = {
+            const value: DataTypes.BookValueType = {
                 ...result.data,
                 state: BookStateTypes.default.STATE_BOOK_IDLE,
                 holder: -1,
             };
-            await axios.put(`${urlBooks} / ${bookId}`, value).catch(error => {
+
+            await axios.put(bookUrl, value).catch(error => {
                 onError(error);
             });
         })
