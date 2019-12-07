5883273f43e0142ef374fa47531c48b90087d201
amogos <iulia.enes5@gmail.com>
2019-12-07 21:04:28 +0100

    wip

--- src/frontend/components/user_feed/user_feed_component.tsx
+++ src/frontend/components/user_feed/user_feed_component.tsx
@@ -8,15 +8,25 @@ interface Props {
     history: any;
 }
 
+const FeedItem = (props: { item: DataTypes.UserFeedRecordType }) => {
+    const { item } = props;
+
+    switch (item.id) {
+        case DataTypes.UserFeedType.LIKES_BOOK: {
+            return (
+                <div>
+                    User {item.user.name} likes book {item.bookDescription ? item.bookDescription.title : ''}
+                </div>
+            );
+        }
+        default:
+            return null;
+    }
+};
+
 const UserFeedComponent = (props: Props) => {
     if (props.userfeed === undefined) return null;
-
-    return props.userfeed.map(item => {
-        switch (item.id) {
-            case DataTypes.UserFeedType.LIKES_BOOK: {
-            }
-        }
-    });
+    return props.userfeed.map(item => <FeedItem item={item} />);
 };
 
 export default withRouter(withStyle(UserFeedComponent, 'user_feed_component'));
