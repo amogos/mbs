69360b6f391094fe43958691d10211d4735110c2
amogos <iulia.enes5@gmail.com>
2019-12-07 17:18:44 +0100

    wip

--- src/backend/endpoints/actions/like_book.ts
+++ src/backend/endpoints/actions/like_book.ts
@@ -1,8 +1,10 @@
 import { getBookDescriptionForISBN, updateBookDescription } from './../books_descriptions';
 import * as DataTypes from '../../../shared/types';
+import { addFeed } from './../user_feed';
 
 export async function likeBook(book: DataTypes.BookRecordType, onError: (resultCode: number) => void): Promise<void> {
     const description = await getBookDescriptionForISBN(book.isbn10, book.isbn13, onError);
     description.likes++;
     await updateBookDescription(description.id, description, onError);
+    await addFeed(DataTypes.UserFeedBookEvent(book.id, DataTypes.UserFeedType.LIKES_BOOK), onError);
 }
