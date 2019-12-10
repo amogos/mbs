cce33dea7cb9ad3bef9a5f1e5770e6ee9b558b7e
amogos <iulia.enes5@gmail.com>
2019-12-10 14:57:23 +0100

    wip

--- src/backend/endpoints/actions/return_book.ts
+++ src/backend/endpoints/actions/return_book.ts
@@ -3,23 +3,20 @@ import { urlBooks } from '../constants';
 import * as BookStateTypes from '../../../shared/constants/book_states_constant';
 import * as DataTypes from './../../../shared/types';
 import { addReturnNotification } from './../return_notifications';
+import { getBookRawRecordTypeFromId } from './../books';
 
 export async function returnBook(bookId: number, onError: (resultCode: number) => void) {
     const bookUrl = `${urlBooks}/${bookId}`;
-    await axios
-        .get(bookUrl)
-        .then(async result => {
-            //  update book record
-            const value: DataTypes.BookRawValueType = result.data;
-            value.state = BookStateTypes.default.STATE_BOOK_IDLE;
-            value.holder = -1;
-            await axios.put(bookUrl, value).catch(error => onError(error));
-            //  add return notification
-            const returnNotification: DataTypes.ReturnNotificationValueType = DataTypes.NullReturnNotificationValueType();
-            returnNotification.bookId = result.data.id;
-            returnNotification.ownerId = result.data.owner;
-            returnNotification.userId = result.data.holder;
-            await addReturnNotification(returnNotification, onError);
-        })
-        .catch(error => onError(error));
+    //  get book raw record
+    let value: DataTypes.BookRawRecordType = await getBookRawRecordTypeFromId(bookId, onError);
+    value = { ...value, state: BookStateTypes.default.STATE_BOOK_IDLE, holder: -1 };
+    //  update record holder
+    await axios.put(bookUrl, value).catch(error => onError(error));
+
+    //  add return notification
+    const returnNotification: DataTypes.ReturnNotificationValueType = DataTypes.NullReturnNotificationValueType();
+    returnNotification.bookId = value.id;
+    returnNotification.ownerId = value.owner;
+    returnNotification.userId = value.holder;
+    await addReturnNotification(returnNotification, onError);
 }
