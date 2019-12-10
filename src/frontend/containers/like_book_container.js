eef3d4c5efb21ab822f28521b82b88a0588b76c5
amogos <iulia.enes5@gmail.com>
2019-12-10 11:33:05 +0100

    wip

--- src/frontend/containers/like_book_container.js
+++ src/frontend/containers/like_book_container.js
@@ -10,4 +10,4 @@ const mapDispatchToProps = dispatch => {
     };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(LikeBookComponent);
+export default connect(null, mapDispatchToProps)(LikeBookComponent);
