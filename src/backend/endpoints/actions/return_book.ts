f5bd351aaec92480cc2d47247a1108b94be2f272
amogos <iulia.enes5@gmail.com>
2019-12-11 05:19:40 +0100

    wip

--- src/backend/endpoints/actions/return_book.ts
+++ src/backend/endpoints/actions/return_book.ts
@@ -6,17 +6,19 @@ import { addReturnNotification } from './../return_notifications';
 import { getBookRawRecordTypeFromId } from './../books';
 
 export async function returnBook(bookId: number, onError: (resultCode: number) => void) {
-    const bookUrl = `${urlBooks}/${bookId}`;
-
     //  get book raw record
-    let value: DataTypes.BookRawRecordType = await getBookRawRecordTypeFromId(bookId, onError);
-    value = { ...value, state: BookStateTypes.default.STATE_BOOK_IDLE, holder: -1 };
-
+    const bookUrl = `${urlBooks}/${bookId}`;
+    let bookRawRecord: DataTypes.BookRawRecordType = await getBookRawRecordTypeFromId(bookId, onError);
     //  update record holder
-    await axios.put(bookUrl, value).catch(error => onError(error));
-
+    bookRawRecord = { ...bookRawRecord, state: BookStateTypes.default.STATE_BOOK_IDLE, holder: -1 };
+    await axios.put(bookUrl, bookRawRecord).catch(error => onError(error));
     //  add return notification
     let returnNotification: DataTypes.ReturnNotificationValueType = DataTypes.NullReturnNotificationValueType();
-    returnNotification = { ...returnNotification, bookId: value.id, ownerId: value.owner, userId: value.holder };
+    returnNotification = {
+        ...returnNotification,
+        bookId: bookRawRecord.id,
+        ownerId: bookRawRecord.owner,
+        userId: bookRawRecord.holder,
+    };
     await addReturnNotification(returnNotification, onError);
 }
