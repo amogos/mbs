b03e0f327dea9008b90af1796616b2ba89d157f4
amogos <iulia.enes5@gmail.com>
2019-12-17 09:49:07 +0100

    wip

--- src/frontend/components/settings/category_settings_component.tsx
+++ src/frontend/components/settings/category_settings_component.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Popover } from 'antd';
+import { Card, Col, Row } from 'antd';
 import * as DataTypes from '../../../shared/types';
 
 interface Props {
@@ -7,7 +7,29 @@ interface Props {
 }
 
 const CategorySettingsComponent = (props: Props) => {
-    return <p>category settings</p>;
+    const { categories } = props;
+    const rows: any[] = [];
+
+    const columns = (i: number): any[] => {
+        const result: any[] = [];
+        for (let j = i; j < props.categories.length && j < i + 3; j++) {
+            const category = props.categories[j];
+            result.push(
+                <Col span={8}>
+                    <Card title={category.title} bordered={false}>
+                        Card content
+                    </Card>
+                </Col>,
+            );
+        }
+        return result;
+    };
+
+    for (let i = 0; i < categories.length; i += 3) {
+        rows.push(<Row gutter={16}>{columns(i)}</Row>);
+    }
+
+    return <div style={{ background: '#ECECEC', padding: '30px' }}>{rows}</div>;
 };
 
 export default CategorySettingsComponent;
