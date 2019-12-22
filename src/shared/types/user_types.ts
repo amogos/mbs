98f10bfc5ca4b7aa98de3ed5d68fd21bd0279175
amogos <iulia.enes5@gmail.com>
2019-12-22 17:43:05 +0100

    wip

--- src/shared/types/user_types.ts
+++ src/shared/types/user_types.ts
@@ -1,33 +1,5 @@
 import { SocialNetwork } from '../constants/social_networks_constants';
 
-export interface UserRecordType {
-    id: number;
-    name: string | undefined;
-    email: string;
-    picture: string;
-    following: number[];
-    bookmarks: number[];
-    categories: number[];
-    rating: number;
-    socialnetwork: number;
-    password: string;
-}
-
-export const NullUserRecordType = (): UserRecordType => {
-    return {
-        id: 0,
-        name: '',
-        email: '',
-        picture: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-        following: [],
-        bookmarks: [],
-        categories: [],
-        rating: 0,
-        socialnetwork: SocialNetwork.custom,
-        password: '',
-    };
-};
-
 export interface UserValueType {
     name: string | undefined;
     email: string;
@@ -51,5 +23,16 @@ export const NullUserValueType = (): UserValueType => {
         rating: 0,
         socialnetwork: SocialNetwork.custom,
         password: '',
+    };
+};
+
+export interface UserRecordType extends UserValueType {
+    id: number;
+}
+
+export const NullUserRecordType = (): UserRecordType => {
+    return {
+        ...NullUserValueType(),
+        id: 0,
     };
 };
