40dbfb214f155fb7bef4d3904e1084552bda54fc
amogos <iulia.enes5@gmail.com>
2019-12-31 05:58:15 +0100

    wip

--- src/frontend/components/regions/right_component.tsx
+++ src/frontend/components/regions/right_component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Aux, withStyle, requiresLogin } from '../hooks/hooks';
+import debounce from 'lodash.debounce';
+import { requiresLogin } from '../hooks/hooks';
 import * as DataTypes from './../../../shared/types';
 import BookmarksList from './../../containers/list_bookmarks_container';
 import UserFeed from './../../containers/user_feed_container';
@@ -8,13 +9,44 @@ interface Props {
     userdata: DataTypes.UserRecordType;
 }
 
-const RightComponent = (props: Props) => {
-    return (
-        <Aux>
-            <BookmarksList />
-            <UserFeed />
-        </Aux>
-    );
-};
+class RightComponent extends React.Component<Props, {}> {
+    refobj: React.RefObject<HTMLDivElement>;
 
-export default requiresLogin(withStyle(RightComponent, 'right_component'));
+    constructor(props: Props) {
+        super(props);
+        this.refobj = React.createRef<HTMLDivElement>();
+    }
+
+    public componentDidMount() {
+        window.onscroll = debounce(() => this.updateStyle(), 10);
+    }
+
+    public componentDidUpdate() {
+        window.onscroll = debounce(() => this.updateStyle(), 10);
+    }
+
+    private updateStyle() {
+        const element = this.refobj.current;
+        if (!element) return;
+
+        const scrollNeededForFixedStyle = 80;
+        const scrollAmount = element.clientHeight - document.documentElement.scrollTop;
+
+        if (document.documentElement.scrollTop > scrollNeededForFixedStyle && scrollAmount < window.innerHeight) {
+            element.className = 'right_component_fixed';
+        } else {
+            element.className = 'right_component';
+        }
+    }
+
+    public render() {
+        return (
+            <div ref={this.refobj}>
+                <BookmarksList />
+                <UserFeed />
+            </div>
+        );
+    }
+}
+
+export default requiresLogin(RightComponent);
