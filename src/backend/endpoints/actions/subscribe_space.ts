167c84c4c3a46a02c8da3fbcec96095a99e3ba90
amogos <iulia.enes5@gmail.com>
2020-01-25 15:22:22 +0100

    wip

--- src/backend/endpoints/actions/subscribe_space.ts
+++ src/backend/endpoints/actions/subscribe_space.ts
@@ -11,17 +11,8 @@ export async function subscribeSpace(
 ) {
     user.pendingSubscriptions.push(space.id);
     space.pendingUsers.push(user.id);
-
-    let nSucceededSteps = 0;
-
-    await updateUser(user, onError, () => nSucceededSteps++);
-    await updateSpace(space, onError, () => nSucceededSteps++);
-
-    if (nSucceededSteps < 2) {
-        if (onFail) onFail();
-    } else if (onSuccess) {
-        onSuccess();
-    }
+    await updateUser(user, onError, onSuccess, onFail);
+    await updateSpace(space, onError, onSuccess, onFail);
 }
 
 export async function unsubscribeSpace(
@@ -33,6 +24,6 @@ export async function unsubscribeSpace(
 ) {
     user.subscriptions = user.subscriptions.filter((spaceId: number) => spaceId == space.id);
     space.subscribedUsers = space.subscribedUsers.filter((userId: number) => userId == user.id);
-    await updateUser(user, onError, onSuccess);
-    await updateSpace(space, onError, onSuccess);
+    await updateUser(user, onError, onSuccess, onFail);
+    await updateSpace(space, onError, onSuccess, onFail);
 }
