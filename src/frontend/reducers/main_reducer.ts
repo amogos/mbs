bbc75f8a46fed58118389a35c3ee08934f291b3a
amogos <iulia.enes5@gmail.com>
2020-01-28 05:38:12 +0100

    wip

--- src/frontend/reducers/main_reducer.ts
+++ src/frontend/reducers/main_reducer.ts
@@ -6,6 +6,7 @@ import pageReducer from './slices/page_reducer';
 import socialReducer from './slices/social_reducer';
 import spaceReducer from './slices/space_reducer';
 import feedReducer from './slices/feed_reducer';
+import { AppActions, BookAction, PageAction, SocialAction, SpaceAction, NotificationAction } from './../actions';
 
 export function handleError(resultCode: number): void {
     message.error(Strings.MYBOOKSHELVE_OPERATION_FAILED + ' (' + resultCode + ')');
@@ -16,23 +17,23 @@ export function handleSuccess(): void {
 }
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function mainReducer(state = {}, action: any): any {
-    let newState = bookReducer(state, action);
+export default function mainReducer(state = {}, action: AppActions): any {
+    let newState = bookReducer(state, action as BookAction);
     if (newState !== null) return newState;
 
-    newState = feedReducer(state, action);
+    newState = feedReducer(state, action as BookAction);
     if (newState !== null) return newState;
 
-    newState = notificationReducer(state, action);
+    newState = notificationReducer(state, action as NotificationAction);
     if (newState !== null) return newState;
 
-    newState = pageReducer(state, action);
+    newState = pageReducer(state, action as PageAction);
     if (newState !== null) return newState;
 
-    newState = socialReducer(state, action);
+    newState = socialReducer(state, action as SocialAction);
     if (newState !== null) return newState;
 
-    newState = spaceReducer(state, action);
+    newState = spaceReducer(state, action as SpaceAction);
     if (newState !== null) return newState;
 
     return state;
